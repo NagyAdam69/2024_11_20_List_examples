1b3ff26a2f96f1f52f7ebd6f94381f4c801ae385
NagyAdam69 <nagy.adam@diak.szbi-pg.hu>
2024-11-20 13:40:17 +0100

    add 01 and 01 task solution

--- 01_lista_elemeinek_megszamolasa.py
+++ 01_lista_elemeinek_megszamolasa.py
@@ -4,3 +4,14 @@ Határozd meg a lista hosszát egy ciklus segítségével
 (a len() függvény megoldásán kívül használj for ciklusos megoldást is),
 és írd ki!
 """
+
+lista = [5, 8 , 12, 15, 22]
+
+# len függvénnyel
+# print(f"{len(lista)} darab elem van a listában")
+
+darab = 0
+
+for elem in lista:
+    darab += 1
+print(f"{darab} darab elem van a listában.")
--- 02_lista_elemeinek_szorzata.py
+++ 02_lista_elemeinek_szorzata.py
@@ -2,3 +2,11 @@
 Hozz létre egy listát számokkal: [2, 4, 6, 8]. 
 Írd ki a lista elemeinek szorzatát (azaz a számok szorzatát)!
 """
+
+lista = [2, 4, 6, 8]
+szorzat = 1
+
+for szam in lista:
+    szorzat *= szam
+
+print(szorzat)
