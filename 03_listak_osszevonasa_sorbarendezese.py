a93401f1879db314a14a74eb0ee7a8c69f7eea6f
NagyAdam69 <nagy.adam@diak.szbi-pg.hu>
2024-11-20 14:26:23 +0100

    add 03 and 04 solution

--- 03_listak_osszevonasa_sorbarendezese.py
+++ 03_listak_osszevonasa_sorbarendezese.py
@@ -2,6 +2,13 @@
 Hozz létre két listát: [3, 1, 4] és [9, 7, 2]. 
 Egyesítsd a két listát, és rendezd a lista elemeit növekvő sorrendbe!
 """
+lista1 = [3, 1, 4]
+lista2 = [9, 7, 2]
 
-
+nagylista = lista1 + lista2
+nagylista.sort()
+print(nagylista)
 # írasd ki az első és az utolsó elemet!
+
+print(nagylista[0])
+print(nagylista[-1])
--- 04_duplikalt_elemek_eltavolitasa.py
+++ 04_duplikalt_elemek_eltavolitasa.py
@@ -4,3 +4,10 @@ Távolítsd el a duplikált számokat, és írd ki az eredményt!
 """
 
 # set-es megoldás
+
+lista = [1, 1, 2, 2, 3, 4, 5, 5, 6]
+
+for i in lista:
+    if i in lista:
+        lista.remove(i)
+print(lista)
